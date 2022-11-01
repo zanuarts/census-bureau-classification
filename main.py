3e7747a53966302db0c75f92870c0686a8c6aa34
Zanuar <zanuar.er@gmail.com>
2022-11-02 05:43:48 +0700

    init fast api

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # Put the code for your API here.
+from fastapi import FastAPI
+
+# Instantiate the app.
+app = FastAPI()
+
+# Define a GET on the specified endpoint.
+@app.get("/")
+async def say_hello():
+    return {"greeting": "Hello World!"}
