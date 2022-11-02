70348348e4b1ec30e87d37ca2aa422d71788a783
Zanuar <zanuar.er@gmail.com>
2022-11-02 22:26:43 +0700

    update model file

--- starter/ml/model.py
+++ starter/ml/model.py
@@ -1,8 +1,10 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
-
+from sklearn.model_selection import GridSearchCV
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn.ensemble import RandomForestClassifier
 
 # Optional: implement hyperparameter tuning.
-def train_model(X_train, y_train):
+def train_model(x_train, y_train):
     """
     Trains a machine learning model and returns it.
 
@@ -17,8 +19,14 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
+    calibrated_forest = CalibratedClassifierCV(
+        base_estimator=RandomForestClassifier(n_estimators=10))
+    param_grid = {
+        'base_estimator__max_depth': [2, 4, 6, 8]}
+    search = GridSearchCV(calibrated_forest, param_grid, cv=5)
+    search.fit(x_train, y_train)
 
-    pass
+    return search
 
 
 def compute_model_metrics(y, preds):
@@ -43,18 +51,18 @@ def compute_model_metrics(y, preds):
     return precision, recall, fbeta
 
 
-def inference(model, X):
+def inference(model, x):
     """ Run model inferences and return the predictions.
 
     Inputs
     ------
     model : ???
         Trained machine learning model.
-    X : np.array
+    x : np.array
         Data used for prediction.
     Returns
     -------
     preds : np.array
         Predictions from the model.
     """
-    pass
+    return model.predict(x)
