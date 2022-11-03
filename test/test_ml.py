be7bc6aa49815e2c6e3eeab1f4ac32160c93ac02
Zanuar <zanuar.er@gmail.com>
2022-11-03 20:08:20 +0700

    add test to train model

--- test/test_ml.py
+++ test/test_ml.py
@@ -1,3 +1,7 @@
+import sys
+
+sys.path.append("./")
+
 import pandas as pd
 import numpy as np
 import pytest
@@ -5,6 +9,7 @@ import sklearn
 from sklearn.model_selection import train_test_split
 
 from starter.ml.data import process_data
+from starter.ml.model import train_model, inference, compute_model_metrics
 
 
 @pytest.fixture
@@ -23,7 +28,7 @@ def cat_features():
 
 @pytest.fixture
 def data():
-    return pd.read_csv('./../data/census.csv')
+    return pd.read_csv('./data/census.csv')
 
 
 def test_process_data_train(data, cat_features):
@@ -74,13 +79,78 @@ def test_process_data_test(data, cat_features):
     assert isinstance(lb, sklearn.preprocessing._label.LabelBinarizer)
 
 
-def test_train_model():
-    pass
+def test_train_model(data, cat_features):
+    # given
+    data.columns = data.columns.str.replace(' ', '')
+    train, test = train_test_split(data, test_size=0.20)
+    x_train, y_train, encoder, lb = process_data(
+        train,
+        categorical_features=cat_features,
+        label="salary",
+        training=True
+    )
+
+    # when
+    model = train_model(x_train, y_train)
+
+    # then
+    assert model is not None
 
 
-def test_compute_model_metrics():
-    pass
+def test_inference(data, cat_features):
+    # given
+    data.columns = data.columns.str.replace(' ', '')
+    train, test = train_test_split(data, test_size=0.20)
+    x_train, y_train, encoder, lb = process_data(
+        train,
+        categorical_features=cat_features,
+        label="salary",
+        training=True
+    )
+
+    x_test, y_test, encoder, lb = process_data(
+        test,
+        categorical_features=cat_features,
+        label="salary",
+        training=False,
+        encoder=encoder,
+        lb=lb
+    )
+    model = train_model(x_train, y_train)
+
+    # when
+    result = inference(model, x_test)
+
+    # then
+    assert isinstance(result, np.ndarray)
 
 
-def test_inference():
-    pass
+def test_compute_model_metrics(data, cat_features):
+    # given
+    data.columns = data.columns.str.replace(' ', '')
+    train, test = train_test_split(data, test_size=0.20)
+    x_train, y_train, encoder, lb = process_data(
+        train,
+        categorical_features=cat_features,
+        label="salary",
+        training=True
+    )
+
+    x_test, y_test, encoder, lb = process_data(
+        test,
+        categorical_features=cat_features,
+        label="salary",
+        training=False,
+        encoder=encoder,
+        lb=lb
+    )
+    model = train_model(x_train, y_train)
+    result = inference(model, x_test)
+
+    # when
+    precision, recall, fbeta = compute_model_metrics(y_test, result)
+
+    # then
+    assert isinstance(precision, float)
+    assert isinstance(recall, float)
+    assert isinstance(fbeta, float)
