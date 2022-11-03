4c1e7ce3bb53bf35b2b819ae118f14fbf01b491c
Zanuar <zanuar.er@gmail.com>
2022-11-03 23:03:19 +0700

    add config example

--- main.py
+++ main.py
@@ -28,6 +28,26 @@ class Value(BaseModel):
     hours_per_week: int
     native_country: object
 
+    class Config:
+        schema_extra = {
+            "example": {
+                "age": 39,
+                "workclass": "State-gov",
+                "fnlgt": 77516,
+                "education": "Bachelors",
+                "education_num": 13,
+                "marital_status": "Never-married",
+                "occupation": "Adm-clerical",
+                "relationship": "Not-in-family",
+                "race": "White",
+                "sex": "Male",
+                "capital_gain": 2174,
+                "capital_loss": 0,
+                "hours_per_week": 40,
+                "native_country": "United-States",
+            }
+        }
+
 
 # Define a GET on the specified endpoint.
 @app.get("/")
