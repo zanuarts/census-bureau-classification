88f9737b2c0add2f5eff71ac31e2d2fdc5823bc0
Zanuar <zanuar.er@gmail.com>
2022-11-05 21:14:17 +0700

    update post prediction test

--- test/test_api.py
+++ test/test_api.py
@@ -2,9 +2,7 @@ import sys
 
 sys.path.append('./')
 
-import json
 from fastapi.testclient import TestClient
-
 from main import app
 
 client = TestClient(app)
@@ -23,29 +21,7 @@ def test_get_malformed():
 
 def test_post_prediction():
     # given
-    data = json.dumps({
-        "age": 39,
-        "workclass": "State-gov",
-        "fnlgt": 77516,
-        "education": "Bachelors",
-        "education_num": 13,
-        "marital_status": "Never-married",
-        "occupation": "Adm-clerical",
-        "relationship": "Not-in-family",
-        "race": "White",
-        "sex": "Male",
-        "capital_gain": 2174,
-        "capital_loss": 0,
-        "hours_per_week": 40,
-        "native_country": "United-States",
-    })
-
-    # when
-    r = client.post("/predict", data=data)
-
-    # then
-    assert r.status_code == 200
-    assert r.json()["result"] == {
+    data = {
         "age": 39,
         "workclass": "State-gov",
         "fnlgt": 77516,
@@ -62,3 +38,9 @@ def test_post_prediction():
         "native_country": "United-States",
     }
 
+    # when
+    r = client.post("/predict", json=data)
+
+    # then
+    assert r.status_code == 200
+    assert r.json()["result"] == "<=50k"
