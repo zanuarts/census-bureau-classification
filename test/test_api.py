5bd7379584c15b7e1a2679ffa39cbbf0687c2983
Zanuar <zanuar.er@gmail.com>
2022-11-06 11:02:46 +0700

    update test

--- test/test_api.py
+++ test/test_api.py
@@ -19,7 +19,7 @@ def test_get_malformed():
     assert r.status_code != 200
 
 
-def test_post_prediction():
+def test_post_prediction_class_0():
     # given
     data = {
         "age": 39,
@@ -43,7 +43,34 @@ def test_post_prediction():
 
     # then
     assert r.status_code == 200
-    assert r.json()["result"] == "<=50k"
+    assert r.json()["result"] == "<=50K"
+
+
+def test_post_prediction_class_1():
+    # given
+    data = {
+        "age": 31,
+        "workclass": "Private",
+        "fnlgt": 45781,
+        "education": "Masters",
+        "education_num": 14,
+        "marital_status": "Never-married",
+        "occupation": "Prof-specialty",
+        "relationship": "Not-in-family",
+        "race": "White",
+        "sex": "Female",
+        "capital_gain": 14084,
+        "capital_loss": 0,
+        "hours_per_week": 50,
+        "native_country": "United-States"
+    }
+
+    # when
+    r = client.post("/predict", json=data)
+
+    # then
+    assert r.status_code == 200
+    assert r.json()["result"] == ">50K"
 
 def test_post_prediction_malformed():
     # given
